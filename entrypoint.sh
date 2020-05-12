f97196c79bc482b99e3abaf5f587d4fcde77f5b9
rustamgk <rustam.gk@gmail.com>
2020-05-12 13:38:49 +0200

    entrypoint updated (verbose logging via env variable added)

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,5 +8,5 @@ export FLASK_APP=chucknorris_webapp
 exec gunicorn \
   --bind="0.0.0.0:${PORT}" \
   --workers=4 \
-  --log-level=info --capture-output \
+  --log-level=${GUNICORN_LOG_LEVEL:-info} --capture-output \
   "${FLASK_APP}:create_app()"
